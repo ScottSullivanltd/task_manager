7db5a95308013e4f893f0effc398cd175f7da03c
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-19 14:38:05 -0600

    Initial commit

---
+++ db/migrate/20220319194908_create_task.rb
@@ -0,0 +1,9 @@
+class CreateTask < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :string
+      t.string :description
+    end
+  end
+end
